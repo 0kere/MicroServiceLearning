f2ede4145d9cbe3a379386477e0c729be2dbbd2b
0kere <47757214+0kere@users.noreply.github.com>
2022-06-06 12:41:54 +0100

    Jacoco set up

--- src/test/java/com/j/s/galley/program/MicroServiceLearningApplicationTests.java
+++ src/test/java/com/j/s/galley/program/MicroServiceLearningApplicationTests.java
@@ -3,7 +3,7 @@ package com.j.s.galley.program;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MicroServiceLearningApplicationTests {
 
 	@Test
