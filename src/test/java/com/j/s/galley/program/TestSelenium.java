aa4cd6b755a9cc6b7784c99db573c315044ddeb8
0kere <47757214+0kere@users.noreply.github.com>
2022-06-23 11:20:11 +0100

    Updated pom file with new selenium dependency

--- src/test/java/com/j/s/galley/program/TestSelenium.java
+++ src/test/java/com/j/s/galley/program/TestSelenium.java
@@ -1,15 +1,12 @@
 package com.j.s.galley.program;
 
-import jdk.jfr.Timespan;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class TestSelenium {
     public static void main (String[] args)
